0533b187e8cda20fbc625b156dcd6250df30a138
Devilliazer <nc.kurumi17@gmail.com>
2025-01-27 00:49:42 +0200

    гамбергер меню

--- src/script.js
+++ src/script.js
@@ -61,3 +61,22 @@ form.addEventListener("submit", (e) => {
     });
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("menu-toggle");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  menuToggle.addEventListener("click", () => {
+    if (mobileMenu.style.display === "block") {
+      mobileMenu.style.display = "none";
+    } else {
+      mobileMenu.style.display = "block";
+    }
+  });
+
+  // Закриття меню при натисканні на посилання
+  document.querySelectorAll("#mobile-menu a").forEach((link) => {
+    link.addEventListener("click", () => {
+      mobileMenu.style.display = "none";
+    });
+  });
+});
